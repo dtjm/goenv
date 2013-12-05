b2f5ff11a793a586ec3a789842648d4791356efb
Sam Nguyen <sam.nguyen@sendgrid.com>
2013-12-05 10:36:49 -0800

    Update goenv.sh

--- goenv.sh
+++ goenv.sh
@@ -10,13 +10,3 @@ if [ ! -x bin/gocode ]; then
     echo "installing gocode into $GOBIN"
     GOBIN=$GOBIN go get github.com/nsf/gocode
 fi
-
-if [ ! -x bin/gocov ]; then
-    echo "installing gocov into $GOBIN"
-    GOBIN=$GOBIN go get github.com/axw/gocov/gocov
-fi
-
-if [ ! -x bin/gocov-html ]; then
-    echo "installing gocov-html into $GOBIN"
-    GOBIN=$GOBIN go get github.com/matm/gocov-html
-fi
